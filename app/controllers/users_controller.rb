ce6ea0656eca2d6092d154c92383734b294013a8
nano91 <hernanprovostev@outlook.com>
2017-10-02 02:47:24 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "¡Bienvenido al Sistema de GuiproÓpticas!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   #get 'users/new'
 
   get 'static_pages/home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
   root 'static_pages#home'
   resources :users
