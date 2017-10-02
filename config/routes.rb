ad394ac7915eb4cb7011f752f51f38eb3833963e
nano91 <hernanprovostev@outlook.com>
2017-10-02 01:51:34 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
+  #get 'users/new'
 
   get 'static_pages/home'
   get '/signup', to: 'users#new'
   
   root 'static_pages#home'
+  resources :users
+  
+  #rails s -b $IP -p $PORT
 end
